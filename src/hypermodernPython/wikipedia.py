bc8bffc7a966e7b2590b205c7791a8d8722282e8
Francois Audibert <loungagna@me.com>
2020-05-27 10:55:12 -0400

    Adding marshmallow and desret to turn the wikipedia reply into a JSON

--- src/hypermodernPython/wikipedia.py
+++ src/hypermodernPython/wikipedia.py
@@ -1,11 +1,32 @@
 # src/hypermodernPython/wikipedia.py
+from dataclasses import dataclass
+
+import click
+import desert
+import marshmallow
 import requests
 
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL: str = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page():
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        return response.json()
+@dataclass
+class Page:
+    title: str
+    extract: str
+
+
+schema = desert.schema(Page, meta={"unknown": marshmallow.EXCLUDE})
+
+
+def random_page(language: str = "en") -> Page:
+    url = API_URL.format(language=language)
+
+    try:
+        with requests.get(url) as response:
+            response.raise_for_status()
+            data = response.json()
+            return schema.load(data)
+    except (requests.RequestException, marshmallow.ValidationError) as error:
+        message = str(error)
+        raise click.ClickException(message)
